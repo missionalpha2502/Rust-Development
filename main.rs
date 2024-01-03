afd32bdd5727db704dab45b68ed3c325a98e7b07
Praveen Jaiprakash <92042769+missionalpha2502@users.noreply.github.com>
2024-01-03 11:58:15 +0530

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,21 @@
+fn main(){
+    // First if else example//
+    let x = 3;
+    
+    if x<=10 {
+        println!("X is less than or equal to 10.");
+    }else {
+        println!("X is greater than or equal to 10.");
+    }
+
+    // Second if else example//
+
+    let y = 1;
+
+    if y<0 {
+        println!("y is negative");
+    }else if y>0 {
+        println!("y is positive");
+    }
+
+}
